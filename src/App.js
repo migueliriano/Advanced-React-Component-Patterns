52ec01d81138b476a1a4db5c8d01cd3efb94d2e4
Miguel Angel <mliriano@intellisys.com.do>
2017-12-07 10:41:30 -0400

    video-2 Build a Toggle Component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class Toggle extends Component {
+  static defaultProps = {onToggle: () => {}}
+  state = {on: false}
+  toggle = () =>
+    this.setState(
+      ({on}) => ({on: !on}),
+      () => {
+        this.props.onToggle(this.state.on)
+      },
+    )
   render() {
+    const {on} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Switch on={on} onClick={this.toggle} />
+    )
   }
+}
+
+function App() {
+  return (
+    <Toggle
+      onToggle={on => console.log('toggle', on)}
+    />
+  )
+}
+
+/*
+ *
+ *
+ * Below here are irrelevant
+ * implementation details...
+ *
+ *
+ */
+
+function Switch({on, className = '', ...props}) {
+  return (
+    <div className="toggle">
+      <input
+        className="toggle-input"
+        type="checkbox"
+      />
+      <button
+        className={`${className} toggle-btn ${on
+          ? 'toggle-btn-on'
+          : 'toggle-btn-off'}`}
+        aria-expanded={on}
+        {...props}
+      />
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App style={{
+  marginTop: 40,
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  textAlign: 'center',
+}} />, document.getElementById('root'));
 registerServiceWorker();
